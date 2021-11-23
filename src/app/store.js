93f0b62b8962a38f42b664384e5ec7c76d905625
Nadav Litvak <nadavlit11@gmail.com>
2021-11-23 11:00:56 +0200

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import weatherReducer from "../features/weather/weatherSlice";
-
-export const store = configureStore({
-  reducer: {
-    weather: weatherReducer,
-  },
-});
